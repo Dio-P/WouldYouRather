d99539178bd6d23474f9136a31c88754f6b14e08
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2021-12-13 16:37:09 +0000

    I had forgotten to wrap App aroung BrowserRouter

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,12 +1,13 @@
-export const increment = (nr) => {
-    return{
-        type: "INCREMENT",
-        payload : nr
-    }
-}
+// export const increment = (nr) => {
+//     return{
+//         type: "INCREMENT",
+//         payload : nr
+//     }
+// }
 
-export const decrement = () => {
-    return{
-        type: "DECREMENT"
-    }
-}
+// export const decrement = () => {
+//     return{
+//         type: "DECREMENT"
+//     }
+// }
+//////////////////////////////////////////
