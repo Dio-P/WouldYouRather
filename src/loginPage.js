9a0277a29623a809aa2ecef2842696a33f7e0899
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2021-12-20 16:53:29 +0000

    the user list is now presented properly so we can choose from it. I need now to make the select element return a value

--- src/loginPage.js
+++ src/loginPage.js
@@ -11,12 +11,11 @@ const LogInPage= () => {
             <button onClick={()=> dispatch(gettingUsers())}> Test</button>
             <h1>Users {JSON.stringify(Object.values(users)[0].name)}</h1>
             <h1>LogInPage</h1>
-            {/* <select name="userId" > */}
-            <p>This:{users.name}</p>
-                {/* {users.map(user=>(<p>{JSON.stringify(user.name)}</p> */}
-                 {/* <option value={user.name}>{user.name}</option> */}
-                {/* ))} */}
-            {/* </select> */}
+            <select name="userId" >
+                {Object.values(users).map(user=>(
+                 <option value={user.name}>{user.name}</option>
+                ))}
+            </select>
         </div>
     )
 }
