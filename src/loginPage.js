5e8efe55a6166c9db200e1c4a3398ff33b902eff
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2021-12-21 15:23:01 +0000

    the id value is now saved and good

--- src/loginPage.js
+++ src/loginPage.js
@@ -6,12 +6,21 @@ const LogInPage= () => {
     const users = useSelector(state=> state.getUsers)
     const dispatch = useDispatch();
 
+    const login = (event) =>{
+        event.preventDefault()
+        let loginID= event.target.value;
+        console.log("loginID", loginID)
+
+
+    }
+
     return(
         <div>
             <button onClick={()=> dispatch(gettingUsers())}> Test</button>
             <h1>Users {JSON.stringify(Object.values(users)[0].name)}</h1>
             <h1>LogInPage</h1>
-            <select name="userId" >
+            <select name="userId" 
+            onChange={(event)=>login(event)}>
                 {Object.values(users).map(user=>(
                  <option value={user.name}>{user.name}</option>
                 ))}
