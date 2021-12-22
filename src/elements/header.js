fa264ce64dad822c1ce5bc28c79a0b393ee0dfc8
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2021-12-22 18:40:08 +0000

    the header is now appearing

--- src/elements/header.js
+++ src/elements/header.js
@@ -1,10 +1,12 @@
 import "../style/header.css"
 
 const Header = () => {
-    <div id="header">
 
-    </div>
+    return(
+        <div id="header">
 
+        </div>
+    )
 }
 
 export default Header
