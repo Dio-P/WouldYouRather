c44b4231f9fa0d42fd6f407bf204f5128e6ef956
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2021-12-22 18:55:41 +0000

    the login state is now chechek and depending on the outcome the text on the header button changes

--- src/elements/header.js
+++ src/elements/header.js
@@ -1,10 +1,16 @@
+import {  useSelector, useDispatch } from "react-redux";
 import "../style/header.css"
 
 const Header = () => {
 
+    const logedOrNot = useSelector(state=> state.isLogged)
+
     return(
         <div id="header">
-
+            {(logedOrNot==="true")?  
+            <button id="logOnOffBtn">Log Off</button>
+            :
+            <button id="logOnOffBtn">Log In</button>}            
         </div>
     )
 }
