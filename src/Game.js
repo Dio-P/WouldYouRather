f68338dee226ce6eadd6f198a573fee35212d6ea
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2021-12-24 15:40:50 +0000

    header should be rendering on top of the game page

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,11 @@
+import {  useSelector, useDispatch } from "react-redux";
+import { getUsers, gettingUsers, login_id, giveUserDetails, signIn  } from "./Actions";
+import Header from "./elements/header";
+
 const GamePage = () => {
     return(
         <div>
+            <Header/>
             <h1>Game Page</h1>
         </div>
     )
