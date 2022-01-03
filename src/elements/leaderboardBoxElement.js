9f6cb687e55517760b9e030978014b3046ad7d59
Dio Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2022-01-03 16:51:07 +0200

    the boxes are now presented and their format makes sence in relation to what it will be

--- src/elements/leaderboardBoxElement.js
+++ src/elements/leaderboardBoxElement.js
@@ -9,13 +9,18 @@ const LeaderBox = (props) => {
 
 
     return (
-        <div>
+        <div class="box">
             <div class="avatarHolder"> 
                 <img id="userAvatar" src={users[partID].avatarURL} alt="a random user avatar"/>
             </div>
-            <h1>User Name {props.user.name}</h1>
-            <h3>Answered Questions: </h3>
-            <h3>Created Questions: </h3>
+            <div>
+                <h1>User Name {props.user.name}</h1>
+            
+            <div>
+                <h3>Answered Questions: </h3>
+                <h3>Created Questions: </h3>
+            </div>
+            </div>
             <div id="scoreHolder">
                 <div> Score</div>
             </div>
