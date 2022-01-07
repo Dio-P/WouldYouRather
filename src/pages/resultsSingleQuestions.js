82b28bf953327eb29702c476ced4d2038f0dd3ac
Dio Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2022-01-07 16:15:15 +0200

    the number of the total votes and the number of its options votes is rendering correctly

--- src/pages/resultsSingleQuestions.js
+++ src/pages/resultsSingleQuestions.js
@@ -1,3 +1,4 @@
+import { useEffect, useCallback} from "react"
 import {  useSelector, useDispatch } from "react-redux";
 import "../style/allBoxes.css";
 
@@ -5,6 +6,38 @@ const SingleQResults = () => {
     const questionsData = useSelector(state=> state.getQuestions)
     const questionID = useSelector(state=> state.questionID)
     const users = useSelector(state=> state.getUsers)
+
+  
+
+    const optionOneVotes=()=>{
+        console.log("option One voret length is: ", questionsData[questionID].optionOne.votes.length)
+        return questionsData[questionID].optionOne.votes.length
+
+    }
+
+    const optionTwoVotes=()=>{
+        console.log("option two votes length is: ", questionsData[questionID].optionTwo.votes.length)
+        return questionsData[questionID].optionTwo.votes.length
+
+    }
+
+    useEffect(()=>{
+        optionOneVotes();
+        optionTwoVotes();
+        
+
+    }, [])
+
+    // const totalVotes = useCallback(()=>{
+    //     const totalVotes = optionOneVotes() + optionTwoVotes();
+    //     console.log("total votes are: ", totalVotes);
+
+    // }, [optionOneVotes, optionTwoVotes])
+
+    // const calculateTotalVotes = useCallback(()=>{
+    //     const totalVotes = optionOneVotes() + optionTwoVotes()
+
+    // }, [optionOneVotes, optionTwoVotes])
     
     return (
         <div class="box">
@@ -13,13 +46,18 @@ const SingleQResults = () => {
             <div className="avatarHolder"> 
                 <img id="userAvatar" src={users[questionsData[questionID].author].avatarURL} alt="a random user avatar"/>
             </div>
+             {/* you need an active class to be telling you what your answer is */}
             <div>
-                <h5>question1</h5>
+                <h5>{questionsData[questionID].optionOne.text}</h5>
                 <p>results stats</p>
+                <p>{optionOneVotes()} out of {optionOneVotes() + optionTwoVotes()} votes</p>
+
             </div>
             <div>
-                <h5>question2</h5>
+                <h5>{questionsData[questionID].optionTwo.text}</h5>
                 <p>result stats</p>
+                <p>{optionTwoVotes()} out of {optionOneVotes() + optionTwoVotes()} votes</p>
+
             </div>
         </div>
     )
