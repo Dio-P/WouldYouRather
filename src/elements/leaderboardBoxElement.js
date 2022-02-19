e6ec068262a7aa4bfa06a07f4ecdad36af3e654e
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2022-02-19 21:59:38 +0000

    tyiding up, up to now

--- src/elements/leaderboardBoxElement.js
+++ src/elements/leaderboardBoxElement.js
@@ -6,16 +6,16 @@ import "../style/main.css";
 const LeaderBox = (props) => {
     const questionsData = useSelector(state=> state.getQuestions)
     const users = useSelector(state=> state.getUsers)
-    const partID = useSelector(state=> state.logID)
+    const partID = useSelector(state=> state.logID)/////////////
     const [ totalScore, setTotalScore ] = useState(0)
     const [ totalQuestionsAnswered, setTotalQuestionsAnswered ] = useState(0)
     const [ totalQuestionsCreated, setTotalQuestionsCreated ] = useState(0)
 
     useEffect(()=>{
-        console.log("users are!@#$%&*&^%$#@!^#%@$#^$*@", users);
-        console.log("users are!@#$%&*&^%$#@!^#%@$#^$*@", questionsData);
-        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Object.values(questionsData)", Object.values(questionsData));
-        console.log("Object.keys(props.user.answers).length !!!!!!!!!!!!!!!", Object.keys(props.user.answers).length);
+        console.log("users are!@#$%&*&^%$#@!^#%@$#^$*@", users);/////////////
+        console.log("users are!@#$%&*&^%$#@!^#%@$#^$*@", questionsData);/////////////
+        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Object.values(questionsData)", Object.values(questionsData));/////////////
+        console.log("Object.keys(props.user.answers).length !!!!!!!!!!!!!!!", Object.keys(props.user.answers).length);/////////////
         // Object.values(questionsData).map(question => {
             // if(question.optionOne.votes.includes(props.user.id) || question.optionTwo.votes.includes(props.user.id)){
             //     setTotalQuestionsAnswered(totalQuestionsAnswered+1)
@@ -25,7 +25,7 @@ const LeaderBox = (props) => {
         setTotalQuestionsAnswered(Object.keys(props.user.answers).length);
         console.log("+++++++++++++++Object.values(users)[1].questions.length+++++++",Object.values(users)[1].questions.length);/////////////
         setTotalQuestionsCreated( users[props.user.id].questions.length);
-        console.log("userId: ", props.user.id, "questions created: ", users[props.user.id].questions.length);
+        console.log("userId: ", props.user.id, "questions created: ", users[props.user.id].questions.length);/////////////
         
         
         // console.log("+++++++++++++++counter2+++++++", counter);
