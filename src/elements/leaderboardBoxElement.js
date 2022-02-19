f94a8eba29f95839eb3919d2d39222e4c37418af
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2022-02-19 22:01:42 +0000

    tydying up 2

--- src/elements/leaderboardBoxElement.js
+++ src/elements/leaderboardBoxElement.js
@@ -4,7 +4,7 @@ import "../style/main.css";
 
 
 const LeaderBox = (props) => {
-    const questionsData = useSelector(state=> state.getQuestions)
+    const questionsData = useSelector(state=> state.getQuestions)/////////////
     const users = useSelector(state=> state.getUsers)
     const partID = useSelector(state=> state.logID)/////////////
     const [ totalScore, setTotalScore ] = useState(0)
@@ -12,10 +12,11 @@ const LeaderBox = (props) => {
     const [ totalQuestionsCreated, setTotalQuestionsCreated ] = useState(0)
 
     useEffect(()=>{
-        console.log("users are!@#$%&*&^%$#@!^#%@$#^$*@", users);/////////////
-        console.log("users are!@#$%&*&^%$#@!^#%@$#^$*@", questionsData);/////////////
-        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Object.values(questionsData)", Object.values(questionsData));/////////////
-        console.log("Object.keys(props.user.answers).length !!!!!!!!!!!!!!!", Object.keys(props.user.answers).length);/////////////
+        // console.log("users are!@#$%&*&^%$#@!^#%@$#^$*@", users);/////////////
+        // console.log("users are!@#$%&*&^%$#@!^#%@$#^$*@", questionsData);/////////////
+        // console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Object.values(questionsData)", Object.values(questionsData));/////////////
+        // console.log("Object.keys(props.user.answers).length !!!!!!!!!!!!!!!", Object.keys(props.user.answers).length);/////////////
+        
         // Object.values(questionsData).map(question => {
             // if(question.optionOne.votes.includes(props.user.id) || question.optionTwo.votes.includes(props.user.id)){
             //     setTotalQuestionsAnswered(totalQuestionsAnswered+1)
@@ -23,40 +24,12 @@ const LeaderBox = (props) => {
         //     }
         // })
         setTotalQuestionsAnswered(Object.keys(props.user.answers).length);
-        console.log("+++++++++++++++Object.values(users)[1].questions.length+++++++",Object.values(users)[1].questions.length);/////////////
-        setTotalQuestionsCreated( users[props.user.id].questions.length);
-        console.log("userId: ", props.user.id, "questions created: ", users[props.user.id].questions.length);/////////////
-        
-        
-        // console.log("+++++++++++++++counter2+++++++", counter);
+        setTotalQuestionsCreated( users[props.user.id].questions.length);        
     }, [])
 
     useEffect(()=>{
         setTotalScore(totalQuestionsCreated + totalQuestionsAnswered)
     }, [totalQuestionsCreated, totalQuestionsAnswered])
-
-    // const countingScore = () => {
-    //     let questions = Object.values(questionsData);
-    //     let allUsers = Object.values(users);
-    //     let score = 0
-    //     for(user of allUsers){
-    //         console.log("user", user)
-    //         for(question of questions){
-    //             if(question.author === user){
-    //                 score +=1;
-    //                 console.log("score1",score);
-    //             }
-    //             else if(question.optionOne.votes.includes(user)){
-    //                 score +=1;
-    //                 console.log("score2",score);
-    //             }
-    //             else if(question.optionTwo.votes.includes(user)){
-    //                 score +=1;
-    //                 console.log("score3",score);
-    //             }
-    //         }
-    //     }
-    // }
 
 
     return (
