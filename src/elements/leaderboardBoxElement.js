42ed0d61911e17536dce0e8e2d213c75804cd725
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2022-03-03 20:59:15 +0000

    tidying up the leaderboard

--- src/elements/leaderboardBoxElement.js
+++ src/elements/leaderboardBoxElement.js
@@ -3,59 +3,29 @@ import {  useSelector, useDispatch } from "react-redux";
 import "../style/main.css";
 
 
-const LeaderBox = (props) => {
-    const questionsData = useSelector(state=> state.getQuestions)/////////////
+const LeaderBox = ({ user }) => {
+
     const users = useSelector(state=> state.getUsers)
     const partID = useSelector(state=> state.logID)/////////////
-    const [ totalScore, setTotalScore ] = useState(0)
-    const [ totalQuestionsAnswered, setTotalQuestionsAnswered ] = useState(0)
-    const [ totalQuestionsCreated, setTotalQuestionsCreated ] = useState(0)
-    const gettingTotalScores = props.gettingTotalScores;
-
-    useEffect(()=>{
-        // console.log("users are!@#$%&*&^%$#@!^#%@$#^$*@", users);/////////////
-        // console.log("users are!@#$%&*&^%$#@!^#%@$#^$*@", questionsData);/////////////
-        // console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Object.values(questionsData)", Object.values(questionsData));/////////////
-        // console.log("Object.keys(props.user.answers).length !!!!!!!!!!!!!!!", Object.keys(props.user.answers).length);/////////////
-        
-        // Object.values(questionsData).map(question => {
-            // if(question.optionOne.votes.includes(props.user.id) || question.optionTwo.votes.includes(props.user.id)){
-            //     setTotalQuestionsAnswered(totalQuestionsAnswered+1)
-                // +Object.keys(props.user.answers).length !!!!!!!!!!!!!!!
-        //     }
-        // })
-        setTotalQuestionsAnswered(Object.keys(props.user.answers).length);
-        setTotalQuestionsCreated( users[props.user.id].questions.length);        
-    }, [])
-
-    useEffect(()=>{
-        setTotalScore(totalQuestionsCreated + totalQuestionsAnswered)
-    }, [totalQuestionsCreated, totalQuestionsAnswered])
-
-    // useEffect(() => {
-    //     console.log("totalScore", totalScore);
-    //     gettingTotalScores({totalScore, userID: props.user.id})
-        
-    // }, [totalScore])
-
 
     return (
+         
         <div class="box">
+
             <div class="avatarHolder"> 
-                <img id="userAvatar" src={users[props.user.id].avatarURL} alt="a random user avatar"/>
+                <img className="userAvatar" src={user.avatarURL} alt="a random user avatar"/>
             </div>
             <div>
-                <h1>{props.user.name}</h1>
+                <h1>{user.name}</h1>
             
             <div>
-                <h3>Answered Questions: {totalQuestionsAnswered}</h3>
-                <h3>Created Questions: {totalQuestionsCreated}</h3>
+                <h3>Answered Questions: {user.scores.totalQuestionsAnswered}</h3>
+                <h3>Created Questions: {user.scores.totalQuestionsCreated}</h3>
             </div>
             </div>
-            <div id="scoreHolder">
-                <div> Score: {totalScore} </div>
+            <div className="scoreHolder">
+                <h3> Score: {user.scores.totalScore} </h3>
             </div>
-
 
         </div>
     )
--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -1,62 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import {  useSelector, useDispatch } from "react-redux";
+import {  useSelector } from "react-redux";
 import LeaderBox from "../elements/leaderboardBoxElement"
 import Header from "../elements/header";
 import "../style/main.css";
 
 const LeaderboardPage =() => {
-    const questionsData = useSelector(state=> state.getQuestions);
     const users = useSelector(state=> state.getUsers);
-    const partID = useSelector(state=> state.logID);
-    // const [totalScores, setTotalScores] = useState([])
-    // let newTotalScores = [];
-    const [ totalScore, setTotalScore ] = useState(0)
-    const [ totalQuestionsAnswered, setTotalQuestionsAnswered ] = useState(0)
-    const [ totalQuestionsCreated, setTotalQuestionsCreated ] = useState(0)
-
+    const [sortedUsersInOrder, setSortedUsersInOrder] = useState([])
     let sortedUsers = Object.values(users)   
-    // .totalScore
-    ///////
+    
     useEffect(()=>{
-        console.log();
-
-        sortedUsers.map(user => { return user["scores"]= { 
+        
+            console.log("users", users);
+            console.log("sortedUser", sortedUsers);
+            sortedUsers.map(user => { return user["scores"]= { 
             totalQuestionsAnswered: Object.keys(user.answers).length, 
             totalQuestionsCreated: user.questions.length,
             totalScore: (Object.keys(user.answers).length+ user.questions.length)
             }
         })
-        console.log("sortedUsers", sortedUsers);
-
-        // setTotalQuestionsAnswered();
-        // setTotalQuestionsCreated( );        
-    }, [])
-
-    // useEffect(()=>{
-    //     setTotalScore(totalQuestionsCreated + totalQuestionsAnswered)
-    // }, [totalQuestionsCreated, totalQuestionsAnswered])
-
-    /////////
-    
-
-    // useEffect(() => {
-    //     console.log("Object.values(users)", Object.values(users));
-    //     console.log("totalScores.sort", totalScores.sort((a, b) => a.totalScore - b.totalScore).reverse());
-
-    // }, [totalScores])
-
-    // const gettingTotalScores = ({totalScore, userID}) => {
+        let sortedUsersInOrderTemp = sortedUsers.sort((a,b)=>a.scores.totalScore - b.scores.totalScore).reverse();
+        setSortedUsersInOrder(sortedUsersInOrderTemp)
         
-    //     newTotalScores.push({totalScore, userID})
-    //     setTotalScores(newTotalScores) 
-    // }
+    }, [users])
+
 
     return (
         <div>
             <Header/>
             <h1>LeaderBoard Page</h1>
             <div className="main_card_style leaderBoard_box">
-            {sortedUsers.map(user=>(<LeaderBox user={user}/>))}
+            {sortedUsersInOrder.map(user=>(<LeaderBox user={user}/>))}
             </div>
         </div>
     )
